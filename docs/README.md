cf3add9b593c608831533a7908235085ae000645
Daniel Grittner <danielgrittner@web.de>
2024-04-24 17:35:53 +0200

    feat: setup docs infra

---
+++ docs/README.md
@@ -0,0 +1,7 @@
+# Admyral Documentation
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+	logo: <span>Admyral</span>,
+	project: {
+		link: "https://github.com/admyral-Security/admyral",
+	},
+	chat: {
+		link: "https://discord.gg/GqbJZT9Hbf",
+	},
+	docsRepositoryBase: "https://github.com/admyral-Security/admyral",
+	footer: {
+		text: "Admyral Technologies GmbH",
+	},
+};
+
+export default config;
