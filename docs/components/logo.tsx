af5fdf555223fafd2cf50816fa2f87243d6c1fd3
Daniel Grittner <danielgrittner@web.de>
2024-04-25 17:47:33 +0200

    feat(docs): setup tailwindcss for docs, consider dark/light theme for logo

--- docs/components/logo.tsx
+++ docs/components/logo.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image";
 
 export default function Logo() {
-    return (
-        <Image src="/logo_dark_theme.svg" alt="Admyral Logo" width={120}
-        height={20} />
-    );
+	return (
+		<>
+			<Image
+				src="/logo_dark_theme.svg"
+				alt="Admyral Logo"
+				width={120}
+				height={20}
+				className="hidden dark:block"
+			/>
+			<Image
+				src="/logo_light_theme.svg"
+				alt="Admyral Logo"
+				width={120}
+				height={20}
+				className="block dark:hidden"
+			/>
+		</>
+	);
 }
