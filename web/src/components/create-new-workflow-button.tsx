2ef0b4e166394f557d37edda7c664da05a466148
Daniel Grittner <danielgrittner@web.de>
2024-04-29 21:25:13 +0200

    fix: allowfullscreen props of iframe

--- web/src/components/create-new-workflow-button.tsx
+++ web/src/components/create-new-workflow-button.tsx
@@ -112,7 +112,7 @@ export default function CreateNewWorkflowButton({
 								frameBorder="0"
 								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 								referrerPolicy="strict-origin-when-cross-origin"
-								allowFullscreen
+								allowFullScreen
 							></iframe>
 						</Flex>
 
