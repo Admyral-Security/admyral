6c3d0e078c1bacdb973233f9ac3a48d7e0d5a96a
Daniel Grittner <danielgrittner@web.de>
2024-05-20 09:59:34 +0200

    fix: favicon and title for docs

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import Logo from "./components/logo";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
 	logo: <Logo />,
@@ -13,6 +14,26 @@ const config: DocsThemeConfig = {
 	docsRepositoryBase: "https://github.com/admyral-Security/admyral",
 	footer: {
 		text: "Admyral Technologies GmbH",
+	},
+	useNextSeoProps() {
+		return {
+			titleTemplate: "%s - Admyral Docs",
+			description: "Next-gen Security Automation",
+			additionalLinkTags: [
+				{
+					rel: "icon",
+					url: "/favicon-light.ico",
+					href: "/favicon-light.ico",
+					media: "(prefers-color-scheme: light)",
+				},
+				{
+					rel: "icon",
+					url: "/favicon.ico",
+					href: "/favicon.ico",
+					media: "(prefers-color-scheme: dark)",
+				},
+			],
+		};
 	},
 };
 
