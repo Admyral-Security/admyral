e1f24a41fe92ffa63910c618f696661fd9dcfacc
Daniel Grittner <danielgrittner@web.de>
2024-05-24 16:20:54 +0200

    feat: add Phish Report integration unit tests

--- workflow-runner/src/workflow/integration_action/phish_report.rs
+++ workflow-runner/src/workflow/integration_action/phish_report.rs
@@ -10,7 +10,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::collections::HashMap;
 
-const PHISH_REPORT: &str = "PhishReport";
+const PHISH_REPORT: &str = "Phish Report";
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
@@ -44,7 +44,7 @@ impl IntegrationExecutor for PhishReportExecutor {
         context: &context::Context,
         api: &str,
         credential_name: &str,
-        parameters: &HashMap<String, String>,
+        parameters: &HashMap<String, serde_json::Value>,
     ) -> Result<serde_json::Value> {
         let api_key = fetch_api_key(credential_name, context).await?;
 
@@ -121,7 +121,7 @@ async fn get_hosting_contact_information(
     client: &dyn HttpClient,
     api_key: &str,
     context: &context::Context,
-    parameters: &HashMap<String, String>,
+    parameters: &HashMap<String, serde_json::Value>,
 ) -> Result<serde_json::Value> {
     let url = get_string_parameter(
         "url",
@@ -148,7 +148,7 @@ async fn start_takedown(
     client: &dyn HttpClient,
     api_key: &str,
     context: &context::Context,
-    parameters: &HashMap<String, String>,
+    parameters: &HashMap<String, serde_json::Value>,
 ) -> Result<serde_json::Value> {
     let url = get_string_parameter(
         "url",
@@ -162,7 +162,7 @@ async fn start_takedown(
     .expect("url is a required parameter");
 
     let ignore_duplicates = get_bool_parameter(
-        "ignore_dupliactes",
+        "ignore_duplicates",
         PHISH_REPORT,
         "START_TAKEDOWN",
         parameters,
@@ -191,7 +191,7 @@ async fn get_takedown(
     client: &dyn HttpClient,
     api_key: &str,
     context: &context::Context,
-    parameters: &HashMap<String, String>,
+    parameters: &HashMap<String, serde_json::Value>,
 ) -> Result<serde_json::Value> {
     let case_id = get_string_parameter(
         "id",
@@ -213,7 +213,7 @@ async fn close_takedown(
     client: &dyn HttpClient,
     api_key: &str,
     context: &context::Context,
-    parameters: &HashMap<String, String>,
+    parameters: &HashMap<String, serde_json::Value>,
 ) -> Result<serde_json::Value> {
     let case_id = get_string_parameter(
         "id",
@@ -242,4 +242,213 @@ async fn close_takedown(
 
     let api_url = format!("https://phish.report/api/v0/cases/{case_id}/close");
     phish_report_request(client, api_key, &api_url, HttpRequest::Put { body }).await
+}
+
+mod tests {
+    use super::*;
+    use crate::postgres::Database;
+    use async_trait::async_trait;
+    use serde_json::json;
+    use std::sync::Arc;
+
+    struct MockHttpClient;
+    #[async_trait]
+    impl HttpClient for MockHttpClient {
+        async fn get(
+            &self,
+            _url: &str,
+            _headers: HashMap<String, String>,
+            _expected_response_status: u16,
+            _error_message: String,
+        ) -> Result<serde_json::Value> {
+            Ok(json!({
+                "domain": "admyral.dev"
+            }))
+        }
+
+        async fn post(
+            &self,
+            _url: &str,
+            _headers: HashMap<String, String>,
+            _body: PostRequest,
+            _expected_response_status: u16,
+            _error_message: String,
+        ) -> Result<serde_json::Value> {
+            Ok(serde_json::json!({
+                "domain": "admyral.ai"
+            }))
+        }
+    
+    }
+
+    struct MockDb;
+    #[async_trait]
+    impl Database for MockDb {
+        async fn fetch_secret(
+            &self,
+            _workflow_id: &str,
+            _credential_name: &str,
+        ) -> Result<Option<String>> {
+            Ok(Some("{\"API_KEY\": \"some-api-key\"}".to_string()))
+        }
+    }
+
+    struct MockDbUnknownSecret;
+    #[async_trait]
+    impl Database for MockDbUnknownSecret {
+        async fn fetch_secret(
+            &self,
+            _workflow_id: &str,
+            _credential_name: &str,
+        ) -> Result<Option<String>> {
+            Ok(None)
+        }
+    }
+
+    async fn setup(db: Arc<dyn Database>) -> (Arc<MockHttpClient>, context::Context) {
+        let context =
+            context::Context::init("ddd54f25-0537-4e40-ab96-c93beee543de".to_string(), None, db)
+                .await
+                .unwrap();
+        (Arc::new(MockHttpClient), context)
+    }
+
+    #[tokio::test]
+    async fn test_missing_credential() {
+        let (client, context) = setup(Arc::new(MockDbUnknownSecret)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "GET_TAKEDOWN",
+                "credentials",
+                &HashMap::new(),
+            )
+            .await;
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap().to_string(),
+            "Missing credentials for Phish Report."
+        );
+    }
+
+    #[tokio::test]
+    async fn test_get_hosting_contact_information() {
+        let (client, context) = setup(Arc::new(MockDb)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "GET_HOSTING_CONTACT_INFORMATION",
+                "credentials",
+                &hashmap! {
+                    "url".to_string() => json!("admyral.dev")
+                },
+            )
+            .await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), json!({ "domain": "admyral.dev" }));
+    }
+
+    #[tokio::test]
+    async fn test_list_takedowns() {
+        let (client, context) = setup(Arc::new(MockDb)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "LIST_TAKEDOWNS",
+                "credentials",
+                &HashMap::new(),
+            )
+            .await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), json!({ "domain": "admyral.dev" }));
+    }
+
+    #[tokio::test]
+    async fn test_start_takedown() {
+        let (client, context) = setup(Arc::new(MockDb)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "START_TAKEDOWN",
+                "credentials",
+                &hashmap! {
+                    "url".to_string() => json!("admyral.dev")
+                },
+            )
+            .await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), json!({ "domain": "admyral.ai" }));
+
+        let (client, context) = setup(Arc::new(MockDb)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "START_TAKEDOWN",
+                "credentials",
+                &hashmap! {
+                    "url".to_string() => json!("admyral.dev"),
+                    "ignore_duplicates".to_string() => json!(true)
+                },
+            )
+            .await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), json!({ "domain": "admyral.ai" }));
+    }
+
+    #[tokio::test]
+    async fn test_get_takedown() {
+        let (client, context) = setup(Arc::new(MockDb)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "GET_TAKEDOWN",
+                "credentials",
+                &hashmap! {
+                    "id".to_string() => json!("abcdef")
+                },
+            )
+            .await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), json!({ "domain": "admyral.dev" }));
+    }
+
+    #[tokio::test]
+    async fn test_close_takedown() {
+        let (client, context) = setup(Arc::new(MockDb)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "GET_TAKEDOWN",
+                "credentials",
+                &hashmap! {
+                    "id".to_string() => json!("abcdef")
+                },
+            )
+            .await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), json!({ "domain": "admyral.dev" }));
+
+        let (client, context) = setup(Arc::new(MockDb)).await;
+        let result = PhishReportExecutor
+            .execute(
+                &*client,
+                &context,
+                "GET_TAKEDOWN",
+                "credentials",
+                &hashmap! {
+                    "id".to_string() => json!("abcdef"),
+                    "comment".to_string() => json!("this is a comment")
+                },
+            )
+            .await;
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), json!({ "domain": "admyral.dev" }));
+    }
 }
