286414794e7b1ff6b395bff94fdce0ffca87b6ef
Daniel Grittner <danielgrittner@web.de>
2024-07-27 21:01:30 +0200

    make workflow-runner docker image multiplatform

--- workflow-runner/push_to_docker_hub.sh
+++ workflow-runner/push_to_docker_hub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker rmi admyralai/workflow-runner:latest
-docker build . -t admyralai/workflow-runner:latest
+docker build --platform linux/amd64,linux/arm64 . -t admyralai/workflow-runner:latest
 docker push admyralai/workflow-runner:latest
