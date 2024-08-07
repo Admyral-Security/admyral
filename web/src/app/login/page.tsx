30acef76d74d13f1722aa8e767cb0bdad2d963f9
Daniel Grittner <danielgrittner@web.de>
2024-08-07 09:47:03 +0200

    fix: selfhosting should not display cloud offline screen

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -78,6 +78,8 @@ function LoginPageChild() {
 		}
 	};
 
+	const isOffline = process.env.NEXT_PUBLIC_IS_CLOUD_OFFLINE === "true";
+
 	return (
 		<Flex
 			direction="column"
@@ -98,278 +100,295 @@ function LoginPageChild() {
 					</Flex>
 				</div>
 
-				{/* <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-					<ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-						<li className="me-2">
-							<button
-								type="button"
-								onClick={() => {
-									resetError();
-									setIsSignIn(true);
-								}}
-								disabled={isSignIn || isLoading}
-								className={
-									isSignIn
-										? "inline-flex items-center justify-center p-4 text-blue-800 border-b-2 border-blue-800 rounded-t-lg active dark:text-blue-800 dark:border-blue-800 group"
-										: "inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-								}
-								aria-current={isSignIn ? "page" : undefined}
-							>
-								Log in
-							</button>
-						</li>
-						<li className="me-2">
-							<button
-								type="button"
-								onClick={() => {
-									resetError();
-									setIsSignIn(false);
-								}}
-								disabled={!isSignIn || isLoading}
-								className={
-									!isSignIn
-										? "inline-flex items-center justify-center p-4 text-blue-800 border-b-2 border-blue-500 rounded-t-lg active dark:text-blue-800 dark:border-blue-800 group"
-										: "inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
-								}
-								aria-current={!isSignIn ? "page" : undefined}
-							>
-								Sign up
-							</button>
-						</li>
-					</ul>
-				</div>
+				{isOffline ? (
+					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+						<Flex direction="column" gap="4">
+							<Text>
+								The public cloud version is currently offline.
+								We are working on a new version and will be back
+								online soon.
+							</Text>
 
-				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-					<form className="space-y-4" onSubmit={handleSubmit}>
-						<div>
-							<label htmlFor="email">Business Email</label>
-							<div className="mt-2">
-								<TextField.Root
-									size="3"
-									variant="surface"
-									id="email"
-									name="email"
-									type="email"
-									required
-								/>
-							</div>
+							<Text>
+								Join our Discord for the latest updates.
+							</Text>
+
+							<Link
+								href="https://discord.com/invite/GqbJZT9Hbf"
+								target="_blank"
+								style={{ width: "100%" }}
+							>
+								<Button style={{ width: "100%" }}>
+									<DiscordLogoIcon />
+									<Text>Join discord</Text>
+								</Button>
+							</Link>
+						</Flex>
+					</div>
+				) : (
+					<div>
+						<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+							<ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+								<li className="me-2">
+									<button
+										type="button"
+										onClick={() => {
+											resetError();
+											setIsSignIn(true);
+										}}
+										disabled={isSignIn || isLoading}
+										className={
+											isSignIn
+												? "inline-flex items-center justify-center p-4 text-blue-800 border-b-2 border-blue-800 rounded-t-lg active dark:text-blue-800 dark:border-blue-800 group"
+												: "inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
+										}
+										aria-current={
+											isSignIn ? "page" : undefined
+										}
+									>
+										Log in
+									</button>
+								</li>
+								<li className="me-2">
+									<button
+										type="button"
+										onClick={() => {
+											resetError();
+											setIsSignIn(false);
+										}}
+										disabled={!isSignIn || isLoading}
+										className={
+											!isSignIn
+												? "inline-flex items-center justify-center p-4 text-blue-800 border-b-2 border-blue-500 rounded-t-lg active dark:text-blue-800 dark:border-blue-800 group"
+												: "inline-flex items-center justify-center p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"
+										}
+										aria-current={
+											!isSignIn ? "page" : undefined
+										}
+									>
+										Sign up
+									</button>
+								</li>
+							</ul>
 						</div>
 
-						<div>
-							<label htmlFor="password">Password</label>
-							<div className="mt-2">
-								<TextField.Root
-									size="3"
-									variant="surface"
-									id="password"
-									name="password"
-									type="password"
-									required
-								/>
-							</div>
-						</div>
-
-						{error !== null && (
-							<Callout.Root color="red">
-								<Flex align="center" gap="5">
-									<Callout.Icon>
-										<InfoCircledIcon
-											width="20"
-											height="20"
+						<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+							<form className="space-y-4" onSubmit={handleSubmit}>
+								<div>
+									<label htmlFor="email">
+										Business Email
+									</label>
+									<div className="mt-2">
+										<TextField.Root
+											size="3"
+											variant="surface"
+											id="email"
+											name="email"
+											type="email"
+											required
 										/>
-									</Callout.Icon>
-									<Callout.Text size="2">
-										{error}
-									</Callout.Text>
-								</Flex>
-							</Callout.Root>
+									</div>
+								</div>
+
+								<div>
+									<label htmlFor="password">Password</label>
+									<div className="mt-2">
+										<TextField.Root
+											size="3"
+											variant="surface"
+											id="password"
+											name="password"
+											type="password"
+											required
+										/>
+									</div>
+								</div>
+
+								{error !== null && (
+									<Callout.Root color="red">
+										<Flex align="center" gap="5">
+											<Callout.Icon>
+												<InfoCircledIcon
+													width="20"
+													height="20"
+												/>
+											</Callout.Icon>
+											<Callout.Text size="2">
+												{error}
+											</Callout.Text>
+										</Flex>
+									</Callout.Root>
+								)}
+
+								<div>
+									<Button
+										type="submit"
+										size="4"
+										variant="solid"
+										loading={isLoading}
+										style={{
+											width: "100%",
+											cursor: "pointer",
+										}}
+									>
+										{isSignIn ? "Log in" : "Sign up"}
+									</Button>
+								</div>
+							</form>
+						</div>
+
+						<div className="sm:mx-auto sm:w-full sm:max-w-sm hover:text-blue-800">
+							<Link href="/password/forgot">
+								Forgot password?
+							</Link>
+						</div>
+
+						{(!!!process.env.NEXT_PUBLIC_DISABLE_GOOGLE_AUTH ||
+							!!!process.env.NEXT_PUBLIC_DISABLE_MICROSOFT_AUTH ||
+							!!!process.env.NEXT_PUBLIC_DISABLE_GITHUB_AUTH) && (
+							<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+								<Grid columns="1fr 38px 1fr" justify="center">
+									<Separator my="3" size="4" />
+									<Flex justify="center" align="start">
+										<Text
+											size="4"
+											style={{
+												color: "var(--Neutral-color-Neutral-Alpha-6, rgba(1, 1, 46, 0.13))",
+											}}
+										>
+											or
+										</Text>
+									</Flex>
+									<Separator my="3" size="4" />
+								</Grid>
+							</div>
 						)}
 
-						<div>
-							<Button
-								type="submit"
-								size="4"
-								variant="solid"
-								loading={isLoading}
-								style={{ width: "100%", cursor: "pointer" }}
-							>
-								{isSignIn ? "Log in" : "Sign up"}
-							</Button>
-						</div>
-					</form>
-				</div>
-
-				<div className="sm:mx-auto sm:w-full sm:max-w-sm hover:text-blue-800">
-					<Link href="/password/forgot">Forgot password?</Link>
-				</div>
-
-				{(!!!process.env.NEXT_PUBLIC_DISABLE_GOOGLE_AUTH ||
-					!!!process.env.NEXT_PUBLIC_DISABLE_MICROSOFT_AUTH ||
-					!!!process.env.NEXT_PUBLIC_DISABLE_GITHUB_AUTH) && (
-					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-						<Grid columns="1fr 38px 1fr" justify="center">
-							<Separator my="3" size="4" />
-							<Flex justify="center" align="start">
-								<Text
-									size="4"
+						{!!!process.env.NEXT_PUBLIC_DISABLE_GOOGLE_AUTH && (
+							<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+								<Button
 									style={{
-										color: "var(--Neutral-color-Neutral-Alpha-6, rgba(1, 1, 46, 0.13))",
+										width: "100%",
+										height: "auto",
+										display: "flex",
+										padding: "12px var(--Space-5, 24px)",
+										alignItems: "center",
+										justifyContent: "start",
+										gap: "var(--Space-5, 24px)",
+										alignSelf: "stretch",
+										borderRadius: "var(--Radius-4, 8px)",
+										border: "1px solid var(--Neutral-color-Neutral-5, #E4E4E9)",
+										background: "var(--Panel-solid, #FFF)",
+										boxShadow:
+											"0px 0px 3px 0px rgba(0, 0, 0, 0.08), 0px 2px 3px 0px rgba(0, 0, 0, 0.17)",
+										cursor: "pointer",
 									}}
+									onClick={() => handleOAuthLogin("google")}
+									loading={loadingOAuth === "google"}
 								>
-									or
-								</Text>
-							</Flex>
-							<Separator my="3" size="4" />
-						</Grid>
-					</div>
-				)}
+									<GoogleIcon />
+									<Text
+										size="4"
+										style={{
+											color: "var(--Tokens-Colors-text, #1C2024)",
+										}}
+									>
+										Continue with Google
+									</Text>
+								</Button>
+							</div>
+						)}
 
-				{!!!process.env.NEXT_PUBLIC_DISABLE_GOOGLE_AUTH && (
-					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-						<Button
-							style={{
-								width: "100%",
-								height: "auto",
-								display: "flex",
-								padding: "12px var(--Space-5, 24px)",
-								alignItems: "center",
-								justifyContent: "start",
-								gap: "var(--Space-5, 24px)",
-								alignSelf: "stretch",
-								borderRadius: "var(--Radius-4, 8px)",
-								border: "1px solid var(--Neutral-color-Neutral-5, #E4E4E9)",
-								background: "var(--Panel-solid, #FFF)",
-								boxShadow:
-									"0px 0px 3px 0px rgba(0, 0, 0, 0.08), 0px 2px 3px 0px rgba(0, 0, 0, 0.17)",
-								cursor: "pointer",
-							}}
-							onClick={() => handleOAuthLogin("google")}
-							loading={loadingOAuth === "google"}
-						>
-							<GoogleIcon />
-							<Text
-								size="4"
-								style={{
-									color: "var(--Tokens-Colors-text, #1C2024)",
-								}}
-							>
-								Continue with Google
+						{!!!process.env.NEXT_PUBLIC_DISABLE_MICROSOFT_AUTH && (
+							<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+								<Button
+									style={{
+										width: "100%",
+										height: "auto",
+										display: "flex",
+										padding: "12px var(--Space-5, 24px)",
+										alignItems: "center",
+										justifyContent: "start",
+										gap: "var(--Space-5, 24px)",
+										alignSelf: "stretch",
+										borderRadius: "var(--Radius-4, 8px)",
+										border: "1px solid var(--Neutral-color-Neutral-5, #E4E4E9)",
+										background: "var(--Panel-solid, #FFF)",
+										boxShadow:
+											"0px 0px 3px 0px rgba(0, 0, 0, 0.08), 0px 2px 3px 0px rgba(0, 0, 0, 0.17)",
+										cursor: "pointer",
+									}}
+									onClick={() => handleOAuthLogin("azure")}
+									loading={loadingOAuth === "azure"}
+								>
+									<Image
+										src="/microsoft_logo.svg"
+										alt="Microsoft"
+										width={24}
+										height={24}
+									/>
+									<Text
+										size="4"
+										style={{
+											color: "var(--Tokens-Colors-text, #1C2024)",
+										}}
+									>
+										Continue with Microsoft
+									</Text>
+								</Button>
+							</div>
+						)}
+
+						{!!!process.env.NEXT_PUBLIC_DISABLE_GITHUB_AUTH && (
+							<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+								<Button
+									style={{
+										width: "100%",
+										height: "auto",
+										display: "flex",
+										padding: "12px var(--Space-5, 24px)",
+										alignItems: "center",
+										justifyContent: "start",
+										gap: "var(--Space-5, 24px)",
+										alignSelf: "stretch",
+										borderRadius: "var(--Radius-4, 8px)",
+										border: "1px solid var(--Neutral-color-Neutral-5, #E4E4E9)",
+										background: "var(--Panel-solid, #FFF)",
+										boxShadow:
+											"0px 0px 3px 0px rgba(0, 0, 0, 0.08), 0px 2px 3px 0px rgba(0, 0, 0, 0.17)",
+										cursor: "pointer",
+									}}
+									onClick={() => handleOAuthLogin("github")}
+									loading={loadingOAuth === "github"}
+								>
+									<GithubIcon />
+									<Text
+										size="4"
+										style={{
+											color: "var(--Tokens-Colors-text, #1C2024)",
+										}}
+									>
+										Continue with GitHub
+									</Text>
+								</Button>
+							</div>
+						)}
+
+						<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+							<Text size="2">
+								By continuing you agree to our{" "}
+								<Link href="/terms-of-service">
+									<u>terms of service</u>
+								</Link>{" "}
+								and{" "}
+								<Link href="/privacy-policy">
+									<u>privacy policy</u>
+								</Link>
+								.
 							</Text>
-						</Button>
+						</div>
 					</div>
 				)}
-
-				{!!!process.env.NEXT_PUBLIC_DISABLE_MICROSOFT_AUTH && (
-					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-						<Button
-							style={{
-								width: "100%",
-								height: "auto",
-								display: "flex",
-								padding: "12px var(--Space-5, 24px)",
-								alignItems: "center",
-								justifyContent: "start",
-								gap: "var(--Space-5, 24px)",
-								alignSelf: "stretch",
-								borderRadius: "var(--Radius-4, 8px)",
-								border: "1px solid var(--Neutral-color-Neutral-5, #E4E4E9)",
-								background: "var(--Panel-solid, #FFF)",
-								boxShadow:
-									"0px 0px 3px 0px rgba(0, 0, 0, 0.08), 0px 2px 3px 0px rgba(0, 0, 0, 0.17)",
-								cursor: "pointer",
-							}}
-							onClick={() => handleOAuthLogin("azure")}
-							loading={loadingOAuth === "azure"}
-						>
-							<Image
-								src="/microsoft_logo.svg"
-								alt="Microsoft"
-								width={24}
-								height={24}
-							/>
-							<Text
-								size="4"
-								style={{
-									color: "var(--Tokens-Colors-text, #1C2024)",
-								}}
-							>
-								Continue with Microsoft
-							</Text>
-						</Button>
-					</div>
-				)}
-
-				{!!!process.env.NEXT_PUBLIC_DISABLE_GITHUB_AUTH && (
-					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-						<Button
-							style={{
-								width: "100%",
-								height: "auto",
-								display: "flex",
-								padding: "12px var(--Space-5, 24px)",
-								alignItems: "center",
-								justifyContent: "start",
-								gap: "var(--Space-5, 24px)",
-								alignSelf: "stretch",
-								borderRadius: "var(--Radius-4, 8px)",
-								border: "1px solid var(--Neutral-color-Neutral-5, #E4E4E9)",
-								background: "var(--Panel-solid, #FFF)",
-								boxShadow:
-									"0px 0px 3px 0px rgba(0, 0, 0, 0.08), 0px 2px 3px 0px rgba(0, 0, 0, 0.17)",
-								cursor: "pointer",
-							}}
-							onClick={() => handleOAuthLogin("github")}
-							loading={loadingOAuth === "github"}
-						>
-							<GithubIcon />
-							<Text
-								size="4"
-								style={{
-									color: "var(--Tokens-Colors-text, #1C2024)",
-								}}
-							>
-								Continue with GitHub
-							</Text>
-						</Button>
-					</div>
-				)}
-
-				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-					<Text size="2">
-						By continuing you agree to our{" "}
-						<Link href="/terms-of-service">
-							<u>terms of service</u>
-						</Link>{" "}
-						and{" "}
-						<Link href="/privacy-policy">
-							<u>privacy policy</u>
-						</Link>
-						.
-					</Text>
-				</div> */}
-
-				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-					<Flex direction="column" gap="4">
-						<Text>
-							The public cloud version is currently offline. We
-							are working on a new version and will be back online
-							soon.
-						</Text>
-
-						<Text>Join our Discord for the latest updates.</Text>
-
-						<Link
-							href="https://discord.com/invite/GqbJZT9Hbf"
-							target="_blank"
-							style={{ width: "100%" }}
-						>
-							<Button style={{ width: "100%" }}>
-								<DiscordLogoIcon />
-								<Text>Join discord</Text>
-							</Button>
-						</Link>
-					</Flex>
-				</div>
 			</Flex>
 
 			<Flex
