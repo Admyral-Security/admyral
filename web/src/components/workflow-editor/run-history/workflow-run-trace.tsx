4a8c388defa6a11e1041f963b1f4c4cc59b0dfb4
Daniel Grittner <danielgrittner@web.de>
2024-09-06 18:45:48 +0200

    fix: handle unknown custom action in run history

--- web/src/components/workflow-editor/run-history/workflow-run-trace.tsx
+++ web/src/components/workflow-editor/run-history/workflow-run-trace.tsx
@@ -87,7 +87,8 @@ export default function WorkflowRunTrace({
 											? "Start"
 											: actionsIndex[
 													workflowStep.actionType
-												].displayName}
+												]?.displayName ||
+												workflowStep.actionType}
 									</Text>
 
 									{workflowStep.error !== null && (
