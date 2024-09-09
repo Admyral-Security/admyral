c87d91ae09596dd3e481d82df89f998f8cb3bda2
leon-schmid <leon.cs.schmid@tum.de>
2024-09-09 10:06:20 +0200

    feat: started working on github integration

--- admyral/actions/integrations/compliance/github.py
+++ admyral/actions/integrations/compliance/github.py
@@ -21,7 +21,6 @@ def get_github_enterprise_client(access_token: str, enterprise: str) -> Client:
         },
     )
 
-
 @action(
     display_name="Search Enterprise Audit Logs",
     display_namespace="GitHub",
@@ -101,3 +100,285 @@ def search_github_enterprise_audit_logs(
                 break
 
         return events if limit is None else events[:limit]
+
+
+def get_github_client(access_token: str) -> Client:
+    return Client(
+        base_url="https://api.github.com",
+        headers={
+            "Authorization": f"Bearer {access_token}",
+            "Accept": "application/vnd.github.v3+json",
+        },
+    )
+
+@action(
+    display_name="List Merged PRs for a Repository",
+    display_namespace="GitHub",
+    description="List all merged PRs for a repository",
+    secrets_placeholders=["GITHUB_SECRET"],
+)
+def list_merged_prs(
+    repo_owner: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Owner",
+            description="The owner of the repository",
+        ),
+    ],
+    repo_name: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Name",
+            description="The name of the repository",
+        ),
+    ],
+    start_time: Annotated[
+        str | None,
+        ArgumentMetadata(
+            display_name="Start Time",
+            description="The start time for the cases to list. Must be in ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ).",
+        ),
+    ] = "1970-01-01T00:00:00Z",
+    end_time: Annotated[
+        str | None,
+        ArgumentMetadata(
+            display_name="End Time",
+            description="The end time for the cases to list. Must be in ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ).",
+        ),
+    ] = "2100-01-01T00:00:00Z",
+) -> list[dict[str, JsonValue]]:
+    # https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28
+
+    secret = ctx.get().secrets.get("GITHUB_SECRET")
+    access_token = secret["access_token"]
+
+    with get_github_client(access_token=access_token) as client:
+        params = {
+            "state": "closed",
+            "sort": "updated",
+            "direction": "desc",
+            "per_page": 100,
+        }
+        url = f"/repos/{repo_owner}/{repo_name}/pulls"
+        events = []
+
+        # handle pagination
+        while True:
+            response = client.get(url, params=params)
+            response.raise_for_status()
+            events.extend(response.json())
+
+            if "next" in response.links:
+                url = response.links["next"]["url"]
+            else:
+                break
+
+        return [
+            pr
+            for pr in events
+            if pr["merged_at"] and start_time <= pr["merged_at"] <= end_time
+        ]
+
+
+@action(
+    display_name="Get Commit History for a PR",
+    display_namespace="GitHub",
+    description="List commit history for a PR from most recent to oldest",
+    secrets_placeholders=["GITHUB_SECRET"],
+)
+def list_commit_history_for_pr(
+    repo_owner: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Owner",
+            description="The owner of the repository",
+        ),
+    ],
+    repo_name: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Name",
+            description="The name of the repository",
+        ),
+    ],
+    pr_number: Annotated[
+        int,
+        ArgumentMetadata(
+            display_name="PR Number",
+            description="The name of the repository",
+        ),
+    ],
+    limit: Annotated[
+        str | None,
+        ArgumentMetadata(
+            display_name="Limit",
+            description="The maximum number of cases to list.",
+        ),
+    ] = 100,
+) -> list[dict[str, JsonValue]]:
+    # https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28
+
+    secret = ctx.get().secrets.get("GITHUB_SECRET")
+    access_token = secret["access_token"]
+
+    with get_github_client(access_token=access_token) as client:
+        params = {
+            "per_page": 100,
+        }
+        url = f"/repos/{repo_owner}/{repo_name}/pulls/{pr_number}/commits"
+        events = []
+
+        # handle pagination
+        while True:
+            response = client.get(url, params=params)
+            response.raise_for_status()
+            events.extend(response.json())
+
+            if "next" in response.links:
+                url = response.links["next"]["url"]
+            else:
+                break
+
+        return sorted(
+            events, key=lambda x: x["commit"]["committer"]["date"], reverse=True
+        )
+
+
+@action(
+    display_name="Get Approval History for a PR",
+    display_namespace="GitHub",
+    description="List aproval history for a PR",
+    secrets_placeholders=["GITHUB_SECRET"],
+)
+def list_approval_history_for_pr(
+    repo_owner: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Owner",
+            description="The owner of the repository",
+        ),
+    ],
+    repo_name: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Name",
+            description="The name of the repository",
+        ),
+    ],
+    pr_number: Annotated[
+        int,
+        ArgumentMetadata(
+            display_name="PR Number",
+            description="The name of the repository",
+        ),
+    ],
+    limit: Annotated[
+        str | None,
+        ArgumentMetadata(
+            display_name="Limit",
+            description="The maximum number of cases to list.",
+        ),
+    ] = 100,
+) -> list[dict[str, JsonValue]]:
+    # https://docs.github.com/en/rest/pulls/reviews?apiVersion=2022-11-28
+    secret = ctx.get().secrets.get("GITHUB_SECRET")
+    access_token = secret["access_token"]
+
+    with get_github_client(access_token=access_token) as client:
+        params = {
+            "per_page": 100,
+        }
+        url = f"/repos/{repo_owner}/{repo_name}/pulls/{pr_number}/reviews"
+        events = []
+
+        # handle pagination
+        while True:
+            response = client.get(url, params=params)
+            response.raise_for_status()
+            events.extend(response.json())
+
+            if "next" in response.links:
+                url = response.links["next"]["url"]
+            else:
+                break
+
+        approves = [review for review in events if review["state"] == "APPROVED"]
+
+        return sorted(approves, key=lambda x: x["submitted_at"], reverse=True)
+
+
+@action(
+    display_name="Get Difference for a Commit",
+    display_namespace="GitHub",
+    description="List diff for a commit",
+    secrets_placeholders=["GITHUB_SECRET"],
+)
+def list_commits(
+    repo_owner: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Owner",
+            description="The owner of the repository",
+        ),
+    ],
+    repo_name: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="Repository Name",
+            description="The name of the repository",
+        ),
+    ],
+    sha: Annotated[
+        str,
+        ArgumentMetadata(
+            display_name="SHA or Branch Name",
+            description="The SHA of the commit",
+        ),
+    ],
+    since: Annotated[
+        str | None,
+        ArgumentMetadata(
+            display_name="Since",
+            description="The start time for the cases to list. Must be in ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ).",
+        ),
+    ] = "1970-01-01T00:00:00Z",
+    until: Annotated[
+        str | None,
+        ArgumentMetadata(
+            display_name="Until",
+            description="The end time for the cases to list. Must be in ISO 8601 format (YYYY-MM-DDTHH:MM:SSZ).",
+        ),
+    ] = "2099-12-31T00:00:00Z",
+    limit: Annotated[
+        str | None,
+        ArgumentMetadata(
+            display_name="Limit",
+            description="The maximum number of cases to list.",
+        ),
+    ] = 100,
+) -> list[dict[str, JsonValue]]:
+    # https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#get-a-commit
+    secret = ctx.get().secrets.get("GITHUB_SECRET")
+    access_token = secret["access_token"]
+
+    with get_github_client(access_token=access_token) as client:
+        params = {
+            "since": since,
+            "until": until,
+            "per_page": 100,
+        }
+        url = f"/repos/{repo_owner}/{repo_name}/commits"
+        events = []
+
+        # handle pagination
+        while True:
+            response = client.get(url, params=params)
+            response.raise_for_status()
+            events.extend(response.json())
+
+            if "next" in response.links:
+                url = response.links["next"]["url"]
+            else:
+                break
+
+        return events
