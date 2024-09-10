103f49fe620cd93073467f5f9e663e7c5f8256bc
leon-schmid <leon.cs.schmid@tum.de>
2024-09-10 11:30:47 +0200

    chore: add GitHub icon to UI for GitHub actions

--- web/src/components/workflow-editor/namespace-icon.tsx
+++ web/src/components/workflow-editor/namespace-icon.tsx
@@ -24,6 +24,7 @@ const NAMESPACE_ICON_MAPPING: Record<string, string> = {
 	Wiz: "/wiz_logo.png",
 	Retool: "/retool_logo.png",
 	"1Password": "/1password_logo.svg",
+	GitHub: "/github_logo.svg",
 };
 
 export default function NamespaceIcon({ namespace }: { namespace: string }) {
