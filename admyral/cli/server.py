8d765124bd101a85a6221590f08fbb0947c9e428
leon-schmid <leon.cs.schmid@tum.de>
2024-09-25 20:19:38 +0200

    fix: change function name for capturing posthog events

--- admyral/cli/server.py
+++ admyral/cli/server.py
@@ -8,7 +8,7 @@ from admyral.utils.docker_utils import (
     get_docker_compose_cmd,
     list_running_docker_containers,
 )
-from admyral.utils.posthog import send_event
+from admyral.utils.posthog import capture
 from admyral.config.config import get_local_postgres_volume
 from admyral import __version__
 
@@ -46,7 +46,7 @@ def up() -> None:
     """
     Launches Admyral services locally.
     """
-    send_event(event_name="CLI", command="up")
+    capture(event_name="Server:up")
 
     click.echo(WELCOME_MESSAGE)
 
@@ -126,7 +126,7 @@ def down() -> None:
     """
     Tears down Admyral services locally.
     """
-    send_event(event_name="CLI", command="down")
+    capture(event_name="Server:down")
 
     if not is_docker_running():
         click.echo(
