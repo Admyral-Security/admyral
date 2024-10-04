1386adceee7c092db03f14e763ca4b2a921839cd
leon-schmid <leon.cs.schmid@tum.de>
2024-10-04 13:41:01 +0200

    chore: adjust descriptions of functions

--- admyral/server/endpoints/action_endpoints.py
+++ admyral/server/endpoints/action_endpoints.py
@@ -31,10 +31,10 @@ async def list_actions(
     authenticated_user: AuthenticatedUser = Depends(authenticate),
 ) -> list[ActionMetadata]:
     """
-    List all stored Python actions.
+    List all stored custom Python actions.
 
     Returns:
-        A list of Python action objects.
+        A list of stored custom Python action objects.
     """
     return await get_admyral_store().list_actions(user_id=authenticated_user.user_id)
 
@@ -44,10 +44,10 @@ async def delete_action(
     action_type: str, authenticated_user: AuthenticatedUser = Depends(authenticate)
 ) -> None:
     """
-    Delete a Python action by its action type.
+    Delete a stored custom Python action by its action type.
 
     Args:
-        action_type: The action type.
+        action_type: function name of the custom Python action.
     """
     await get_admyral_store().delete_action(
         action_type=action_type, user_id=authenticated_user.user_id
